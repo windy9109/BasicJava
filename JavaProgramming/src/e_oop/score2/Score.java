b45bbe9b3560565b7e22e8edd8ceea89d7b44c11
windy9109 <windy9109@naver.com>
2022-01-19 21:43:46 +0900

    메서드 응용예제 - 성적(학생별/과목별)출력하기2

---
+++ JavaProgramming/src/e_oop/score2/Score.java
@@ -0,0 +1,70 @@
+package e_oop.score2;
+
+import e_oop.score2.Student;
+
+public class Score {
+
+	public static void main(String[] args) {
+
+
+		String[] studentNames = {"김기웅","박태정","이정규","오지현"};
+		Student[] students = new Student[studentNames.length];
+		
+		
+		for(int i=0; i< students.length; i++) {
+			students[i] = new Student(); //객체생성
+			students[i].name = studentNames[i]; //배열에 넣기
+			students[i].randomScore(); //점수랜덤 발생
+			students[i].getSum(); //점수합계
+			students[i].getAverage(); //점수평균
+		}
+		
+		for(int i=0; i< students.length; i++) {
+			students[i].getRank(students);
+		}
+		
+		for(int i=0; i< students.length; i++) {
+			int min = i;
+			for(int j=i+1; j< students.length; j++) {
+				if(students[j].rank < students[min].rank) {
+					min = j;
+				}
+			}
+			Student temp = students[i];
+			students[i] = students[min];
+			students[min] = temp;
+		}
+		
+		int[] subSum = new int[3];
+		for(int i=0; i< students.length; i++) {
+			subSum[0] += students[i].kor;
+			subSum[1] += students[i].eng;
+			subSum[2] += students[i].math;
+			
+		}
+		
+		double[] subAvg = new double[3];
+		for(int i=0; i< subAvg.length; i++) {
+			subAvg[i] = Math.round(subSum[i]/students.length*100)/100.0;
+			
+		}
+		
+		
+		System.out.print("이름\t국어\t영어\t수학\t합계\t평균\t석차\n");
+		for(int i=0; i< students.length; i++) {
+			System.out.println(students[i].getInfo());
+		}
+		System.out.print("과목합계");
+		for(int i=0; i< subSum.length; i++) {
+			System.out.print("\t"+subSum[i]);
+		}
+		System.out.println();
+		System.out.print("과목평균");
+		for(int i=0; i< subSum.length; i++) {
+			System.out.print("\t"+subAvg[i]);
+		}
+		
+
+	}
+
+}
---
+++ JavaProgramming/src/e_oop/score2/Student.java
@@ -0,0 +1,56 @@
+package e_oop.score2;
+
+import e_oop.score2.Student;
+
+public class Student {
+
+	//변수선언
+	String name;
+	int kor;
+	int eng;
+	int math;
+	double avg;
+	int rank =1;
+	
+//	int stsum= kor + eng + math;
+//	double stavg = Math.round(stsum/3.0*100)/100.0;
+	
+	
+	
+	//점수랜덤생성
+	int sum;
+	void randomScore() {
+		kor = (int)(Math.random()*101);
+		eng = (int)(Math.random()*101);
+		math = (int)(Math.random()*101);
+	} 
+	
+	//합계
+	int getSum() {
+		return sum = kor+eng+math;
+	}
+	
+	//평균
+	double getAverage() {
+		return avg = Math.round(getSum()/3.0*100)/100.0;
+	}
+	
+	int getRank(Student[] students) {
+		rank=1;
+		for(int i=0; i<students.length; i++) {
+			if(getSum()<students[i].getSum()) {
+				rank++;
+				
+			}
+		}
+		return rank;
+	}
+	
+	
+	//데이터제공 메서드
+	String getInfo() {
+		return name +"\t"+kor+"\t"+eng+"\t"+math+"\t"+sum+"\t"+avg+"\t"+rank+"\t";
+	}
+	
+	
+}
