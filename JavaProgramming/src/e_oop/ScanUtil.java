0e006d2b25053c0935c023d1681e041cd3bea782
windy9109 <windy9109@naver.com>
2022-01-25 16:31:25 +0900

    [JAVA] Scanner 자동 생성자 만들기 & 예외처리

--- JavaProgramming/src/e_oop/ScanUtil.java
+++ JavaProgramming/src/e_oop/ScanUtil.java
@@ -7,13 +7,30 @@ public class ScanUtil {
 	private static Scanner s =new Scanner(System.in);
 	
 	public static String nextLine() {
+		
 		return s.nextLine();
 	}
 	
+	
+	
 	public static int nextInt() {
-		return Integer.parseInt(s.nextLine());
+		int input =0;
+		try {
+		input= Integer.parseInt(s.nextLine());
+	}catch(Exception e) {
+		System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.>");
+		input = nextInt(); //재귀호출
+		}
+		return input;
+	
+	
+	
 	}
 	
 	
 	
 }
+
+
+
+//입력받는 값이 다를때 예외처리
