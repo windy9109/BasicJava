1c23e95c026029f3b34bba0d00bcd7fb0dacee39
windy9109 <windy9109@naver.com>
2022-01-25 12:04:01 +0900

    [JAVA] 접근제한자 - public, protected, private, defalt

---
+++ JavaProgramming/src/g_oop2/AccessMisifier.java
@@ -0,0 +1,65 @@
+package g_oop2;
+
+public class AccessMisifier {
+
+	public String publicVar = "public: 접근제한이 없음";
+	protected String protectedVar
+		="protected: 같은 패키지 + 상속받은 클래스에서 접근 가능";
+	String defaultVar = "default: 같은 패키지 에서만 접근 가능";
+	private String privateVar = "private: 클래스 내에서만 접근가능";
+	
+	public void publicMethod() {
+		System.out.println(publicVar);
+	}
+	protected void protectedMethod() {
+		System.out.println(protectedVar);
+	}
+	void defaultMethod() {
+		System.out.println(defaultVar);
+	}
+	private void privateMethod() {
+		System.out.println(privateVar);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		AccessMisifier am = new AccessMisifier();
+		System.out.println(am.publicVar);
+		am.publicMethod();
+
+		System.out.println(am.protectedVar);
+		am.protectedMethod();
+		
+		System.out.println(am.defaultVar);
+		am.defaultMethod();
+		
+		System.out.println(am.privateVar);
+		am.privateMethod();
+		
+		
+		
+		
+		Time t = new Time();
+		
+		//변수를 통해서 접근함
+//		t.hour =14;
+//		t.minute =9;
+//		t.second = 99;
+		
+		t.setHour(16);
+		t.setMinute(9);
+		t.setSecond(-7);
+		
+		
+		System.out.println(t);
+		
+		t.clock();
+		
+		
+	}
+	
+	
+	
+	
+}
