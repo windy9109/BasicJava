b9b3b7b4298ea2ea7abfb179e170a7cc30de8004
windy9109 <windy9109@naver.com>
2022-02-07 16:02:06 +0900

    [JAVA] JDBC - INSERT

--- JavaProgramming/src/k_jdbc/Insert.java
+++ JavaProgramming/src/k_jdbc/Insert.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+ 
 public class Insert {
 
 	public static void main(String[] args) {
